7b56a7089981638fe188b3c9ecf6d8dfbcad87f7
Wilreyyt <maxim.lazar.tox@mail.ru>
2024-05-27 16:17:03 +0300

    Plz, mypy, start working

--- homework_7/cows_and_bulls.py
+++ homework_7/cows_and_bulls.py
@@ -8,8 +8,8 @@ RANDOM_NUMBER = ""
 while REPEAT == 1:
     RANDOM_NUMBER = str(random.randint(1000, 9999 + 1))
     REPEAT = 0
-    for i in RANDOM_NUMBER:
-        if RANDOM_NUMBER.count(i) > 1:
+    for character in RANDOM_NUMBER:
+        if RANDOM_NUMBER.count(character) > 1:
             REPEAT = 1
 INPUT_NUMBER = ""
 while INPUT_NUMBER != RANDOM_NUMBER:
@@ -21,8 +21,8 @@ while INPUT_NUMBER != RANDOM_NUMBER:
                 BULLS_COUNT += 1
         print("Быки:", BULLS_COUNT)
         COWS_COUNT = -BULLS_COUNT
-        for i in INPUT_NUMBER:
-            if i in RANDOM_NUMBER:
+        for j in INPUT_NUMBER:
+            if j in RANDOM_NUMBER:
                 COWS_COUNT += 1
         print("Коровы:", COWS_COUNT)
     else:

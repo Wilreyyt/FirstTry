2df23c4acffc0c60733f8dc43858b19a1a05d68f
Wilreyyt <maxim.lazar.tox@mail.ru>
2024-05-29 22:32:58 +0300

    Trying to unable C103

--- homework_7/cows_and_bulls.py
+++ homework_7/cows_and_bulls.py
@@ -1,4 +1,5 @@
 """Cows and bulls"""
+# pylint: disable-msg=C0103
 
 import random
 
@@ -11,20 +12,26 @@ while REPEAT == 1:
     for character in RANDOM_NUMBER:
         if RANDOM_NUMBER.count(character) > 1:
             REPEAT = 1
+
 INPUT_NUMBER = ""
 while INPUT_NUMBER != RANDOM_NUMBER:
-    INPUT_NUMBER = input("Введите ваше 4-значное число: ")
-    if len(INPUT_NUMBER) == 4:
-        BULLS_COUNT = 0
-        for i in range(4):
-            if INPUT_NUMBER[i] == RANDOM_NUMBER[i]:
-                BULLS_COUNT += 1
-        print("Быки:", BULLS_COUNT)
-        COWS_COUNT = -BULLS_COUNT
-        for j in INPUT_NUMBER:
-            if j in RANDOM_NUMBER:
-                COWS_COUNT += 1
-        print("Коровы:", COWS_COUNT)
-    else:
-        print("Введите число заново!")
+    REPEAT = 1
+    while REPEAT == 1 or len(INPUT_NUMBER) != 4:
+        INPUT_NUMBER = input("Введите ваше 4-значное число: ")
+        REPEAT = 0
+        for character in INPUT_NUMBER:
+            if INPUT_NUMBER.count(character) > 1 or character < '0' or character > '9':
+                REPEAT = 1
+
+    BULLS_COUNT = 0
+    for i in range(4):
+        if INPUT_NUMBER[i] == RANDOM_NUMBER[i]:
+            BULLS_COUNT += 1
+    print("Быки:", BULLS_COUNT)
+
+    COWS_COUNT = -BULLS_COUNT
+    for j in INPUT_NUMBER:
+        if j in RANDOM_NUMBER:
+            COWS_COUNT += 1
+    print("Коровы:", COWS_COUNT)
 print("Вы выиграли!")

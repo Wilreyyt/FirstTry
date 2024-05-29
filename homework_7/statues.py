e3a7db4a236441bb7b7610ff9b6088e332bc203f
Wilreyyt <maxim.lazar.tox@mail.ru>
2024-05-29 22:26:58 +0300

    New fix, exept uppercase

--- homework_7/statues.py
+++ homework_7/statues.py
@@ -7,10 +7,9 @@ for i in range(4):
     while STATUE == 0 or STATUE in STATUES:
         STATUE = random.randint(1, 10)
     STATUES.append(STATUE)
-STATUES.sort()
 print(STATUES)
 MISSING_STATUES = 0
-for i in range(STATUES[0], STATUES[-1]):
+for i in range(min(STATUES), max(STATUES)):
     if i not in STATUES:
         MISSING_STATUES += 1
 print("Кол-во пропущенных статуй: ", MISSING_STATUES)

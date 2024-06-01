a3bc4f63b4ecb33c1ea18f6135732539ec1a9797
Wilreyyt <maxim.lazar.tox@mail.ru>
2024-06-01 14:52:00 +0300

    Fix linters 8

--- homework_8/opposite_number.py
+++ homework_8/opposite_number.py
@@ -6,7 +6,7 @@ def find_opposite_number(first_number, n):
     """Function printing python version."""
     if first_number >= n or first_number < 0:
         print("Данное число не лежит на окружности")
-        return
+        return None
     if first_number > n / 2 - 1:
         opposite_number = int(first_number - n / 2)
     else:
@@ -20,7 +20,7 @@ def main():
     n = int(input())
     if n % 2 != 0 or n <= 0:
         print("Ошибка! Введите число заново")
-        return
+        return None
     first_number = int(input("Введите любое целое число на окружности: "))
     opposite_number = find_opposite_number(first_number, n)
     print("Число напротив будет: ", opposite_number)

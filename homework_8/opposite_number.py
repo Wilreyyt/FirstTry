909b440da305ad565e329058b5cb6a8cd340a10b
Wilreyyt <maxim.lazar.tox@mail.ru>
2024-06-01 13:21:06 +0300

    Opposite number is done

--- homework_8/opposite_number.py
+++ homework_8/opposite_number.py
@@ -1,13 +1,7 @@
 """Opposite number"""
 # pylint: disable-msg=C0103
 
-def main():
-    print("Введите количество чисел на окружности (обязательно четное!): ")
-    n = int(input())
-    if n %2 != 0 or n <= 0:
-        print("Ошибка! Введите число заново")
-        return
-    first_number = int(input("Введите любое целое число на окружности: "))
+def find_opposite_number(first_number, n):
     if first_number >= n or first_number < 0:
         print("Данное число не лежит на окружности")
         return
@@ -15,5 +9,14 @@ def main():
         opposite_number = int(first_number - n / 2)
     else:
         opposite_number = int(first_number + n / 2)
+    return opposite_number
+def main():
+    print("Введите количество чисел на окружности (обязательно четное!): ")
+    n = int(input())
+    if n %2 != 0 or n <= 0:
+        print("Ошибка! Введите число заново")
+        return
+    first_number = int(input("Введите любое целое число на окружности: "))
+    opposite_number = find_opposite_number(first_number, n)
     print("Число напротив будет: ", opposite_number)
 main()

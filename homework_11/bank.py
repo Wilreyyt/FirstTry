c8245ef4096ec20d6e85ba75721c88c257d0dc47
Wilreyyt <maxim.lazar.tox@mail.ru>
2024-06-15 19:53:16 +0300

    HW11

---
+++ homework_11/bank.py
@@ -0,0 +1,41 @@
+"""Bank"""
+
+
+from calendar import monthrange
+
+
+class Deposit:
+    def __init__(self, size: float, term_months: int):
+        self.size = size
+        self.term_months = term_months
+
+
+class Bank:
+    def __init__(self, percent: int):
+        self.percent = percent
+
+    def deposit(self, money, term_years):
+        deposit = Deposit(money, term_years * 12)
+        percent_coefficient = self.percent / 100
+
+        result_money = deposit.size
+
+        for i in range(deposit.term_months):
+            days_in_month = monthrange(2024, i % 12 + 1)[1]
+            result_money += round(result_money * percent_coefficient / 366 * days_in_month, 2)
+
+        return round(result_money, 2)
+
+
+def main():
+    percent = 10
+    bank = Bank(percent)
+
+    money = int(input('Введите количество денежных средств для вклада: '))
+    term = int(input('Введите срок (лет): '))
+
+    revenue = bank.deposit(money, term)
+    print('Количество полученных средств:', revenue)
+
+
+main()

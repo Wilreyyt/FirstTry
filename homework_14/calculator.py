913d87c7757f8c7d2d7a77779f3eb417f0193b4d
Wilreyyt <maxim.lazar.tox@mail.ru>
2024-06-26 17:46:15 +0300

    HW 14

---
+++ homework_14/calculator.py
@@ -0,0 +1,139 @@
+"""Калькулятор"""
+
+
+from enum import Enum
+
+
+OPERATORS_TO_PRIORITY = {
+    "**": 1,
+    "*": 2,
+    "/": 2,
+    "+": 3,
+    "-": 3,
+}
+
+
+class LexemeTypes(Enum):
+    """Типы лексем"""
+    operator = "operator",
+    number = "number"
+
+
+class Lexeme:
+    """Обозначает символ в выражении - число или оператор"""
+    def __init__(self, value: str, lexeme_type: LexemeTypes):
+        self.value = value
+        self.type = lexeme_type
+
+
+def parse_lexemes(lexemes: list[str]) -> list[Lexeme]:
+    """Переводит строковые значения символов в лексемы"""
+    result = []
+    for lexeme in lexemes:
+        if lexeme in OPERATORS_TO_PRIORITY:
+            lexeme = Lexeme(lexeme, LexemeTypes.operator)
+        else:
+            number = float(lexeme)
+            lexeme = Lexeme(str(number), LexemeTypes.number)
+
+        result.append(lexeme)
+
+    return result
+
+
+def validate_lexemes(lexemes: list[Lexeme]) -> bool:
+    """Проверяет правильность ввода пользователя"""
+    expected_type = LexemeTypes.number
+
+    for lexeme in lexemes:
+        if lexeme.type != expected_type:
+            return False
+
+        if expected_type == LexemeTypes.number:
+            expected_type = LexemeTypes.operator
+        elif expected_type == LexemeTypes.operator:
+            expected_type = LexemeTypes.number
+        else:
+            return False
+
+    return True
+
+
+def calculate_operator(a: float, b: float, operator: str) -> float:
+    """Вычислить значения, используя строку оператора"""
+    if operator == '**':
+        return a ** b
+    if operator == '*':
+        return a * b
+    if operator == '/':
+        return a / b
+    if operator == '+':
+        return a + b
+    if operator == '-':
+        return a - b
+    
+    raise ValueError("Некорректный оператор")
+
+
+def resolve_operator(lexemes: list[Lexeme], priority: int) -> bool:
+    """Выполнить вычисление одного оператора в списке лексем"""
+    operator = None
+    i = 0
+    for lexeme in lexemes:
+        if lexeme.type == LexemeTypes.operator \
+            and OPERATORS_TO_PRIORITY[lexeme.value] == priority:
+            operator = lexeme.value
+            break
+        i += 1
+
+    if operator is not None:
+        a = float(lexemes[i - 1].value)
+        b = float(lexemes[i + 1].value)
+
+        operator_result = calculate_operator(a, b, operator)
+
+        lexemes[i] = Lexeme(str(operator_result), LexemeTypes.number)
+        del lexemes[i + 1]
+        del lexemes[i - 1]
+
+        return True
+    
+    return False
+
+
+def calculate_result(expression: str) -> float:
+    """Калькулятор"""
+    lexemes = parse_lexemes(expression.split())
+    if not validate_lexemes(lexemes):
+        raise ValueError("Выражение некорректно")
+    
+    first_priority = min(OPERATORS_TO_PRIORITY.values())
+    last_priority = max(OPERATORS_TO_PRIORITY.values())
+
+    for priority in range(first_priority, last_priority + 1):
+        next_available = True
+        while next_available:
+            next_available = resolve_operator(lexemes, priority)
+                
+
+    return float(lexemes[0].value)
+
+
+def main():
+    """Основной код программы"""
+    expression = input("Введите простое математическое выражение: ")
+
+    try:
+        result = calculate_result(expression)
+    except Exception:
+        print("Произошла ошибка ввода!")
+        return
+
+    print(result)
+
+
+assert calculate_result("-6 + 5") == -1.0
+assert calculate_result("6 ** 2 / 2 + 2") == 20.0
+assert calculate_result("7 + 5 * 4 / 2") == 17.0
+
+main()
---
+++ homework_14/files.py
@@ -0,0 +1,111 @@
+"""Файлы"""
+
+
+import os.path
+
+
+class Student:
+    """Студент"""
+    def __init__(self, name: str, group: str, grades: list[int]):
+        self.name = name
+        self.group = group
+        self.grades = grades
+
+    def to_str(self) -> str:
+        """Преобразовать студента в строку"""
+        return f"{self.name} | {self.group} | {self.grades}\n"
+
+
+def read_student_from_string(string: str) -> Student:
+    """Создать студента из строки"""
+    words = string.split("|")
+
+    str_grades = words[2].strip()
+    str_grades = str_grades[1:-1]
+
+    grades = str_grades.split(',')
+    result_grades = []
+    for grade in grades:
+        result_grades.append(int(grade.strip()))
+
+    return Student(
+        words[0].strip(),
+        words[1].strip(),
+        result_grades
+    )
+
+
+def get_count_by_groups(studens: list[Student]) -> dict[str, int]:
+    """Посчитать количество студентов по группам"""
+    result = dict()
+    for student in studens:
+        if student.group not in result:
+            result[student.group] = 0
+        
+        result[student.group] += 1
+    
+    return result
+
+
+def read_students_from_file(file_path: str) -> list[Student]:
+    """Прочитать студентов из файла"""
+    students_from_file = []
+    with open(file_path, "r") as file:
+        for line in file.readlines():
+            if line == '\n':
+                continue
+            if line.startswith("Number of students"):
+                return students_from_file
+
+            student = read_student_from_string(line)
+            students_from_file.append(student)
+
+    return students_from_file
+
+
+def main():
+    """Основной код программы"""
+    file_path = "students.txt"
+
+    students = [
+        Student("Maxim Lazar", "Group 1", [7, 8, 6, 7, 9]),
+        Student("Kurnos Kuzovatov", "Group 1", [4, 7, 3, 5, 8, 7, 6]),
+        Student("Ernest Hemingway", "Group 1", [7, 7, 7, 5, 9]),
+        Student("Stepan Bogdan", "Group 2", [10, 8, 5, 6, 7, 8, 10]),
+        Student("Vladimir Trekatol", "Group 2", [6, 7, 9, 9]),
+    ]
+    
+    if not os.path.exists(file_path):
+        print("Запись в файл")
+        with open(file_path, "w") as file:
+            for student in students:
+                file.write(student.to_str())
+
+    print("Чтение из файла")
+    try:
+        students_from_file = read_students_from_file(file_path)
+    except:
+        print("Произошла ошибка при чтении студентов из файла. Некорректный формат")
+        return
+    
+    print("Число студентов в файле:", len(students_from_file))
+
+    students_by_groups = get_count_by_groups(students_from_file)
+    print("Количество студентов по группам:", students_by_groups)
+
+    has_statistics = False
+    with open(file_path, "r") as file:
+        for line in file.readlines():
+            if "Number of students:" in line:
+                has_statistics = True
+
+    if not has_statistics:
+        with open(file_path, "a") as file:
+            file.write(f"\nNumber of students: {len(students_from_file)}\n\n")
+
+            file.write("Student count by groups\n")
+            for group, count in students_by_groups.items():
+                file.write(f"{group}: {count}\n")
+
+
+main()
